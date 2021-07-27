8ee816d82c5f7052fa1f7dd6f6ef08e6ef08d5e4
Sujata-sampa <sujata.sampa8@gmail.com>
2021-07-27 19:20:07 +0530

    function

---
+++ Challenge3/Get-Value.ps1
@@ -0,0 +1,18 @@
+﻿function Get-Value
+{
+  @{'a' = @{'b' = @{'c' = 'd'}}}
+
+
+$items | Format-Table Name, @{n='Value';e={
+  if ($_.Value -is [Hashtable]) {
+    $ht = $_.Value
+    $a  = $ht.keys | % { '{0}={1}' -f $_, $ht[$_] }
+    '{{{0}}}' -f ($a -join ', ')
+  } else {
+    $_.Value
+  }
+}}
+
+}
+
+
